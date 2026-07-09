fb55d075254109008a9b0d6b55f4a028bb0ff4ba
RINA708 <naopuccho708@gmail.com>
2026-07-09 15:38:55 +0900

    Add mouseover event for butterfly and foo elements

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+document.querySelectorAll('.butterfly').forEach(function (el) {
+  el.addEventListener('mouseover', function () {
+    var x = Math.random() * 850;
+    var y = Math.random() * 430;
+    el.style.left = x + 'px';
+    el.style.top  = y + 'px';
+  });
+});
 
+document.querySelectorAll('.foo').forEach(function (el) {
+  el.addEventListener('mouseover', function () {
+    var x = 360 + Math.random() * 430;
+    var y = 190 + Math.random() * 70;
+    el.style.left = x + 'px';
+    el.style.top  = y + 'px';
+  });
+});
